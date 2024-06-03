9f55c4dae43bad101a4f1e0a533a3506db67d04a
6EmptyBucket9 <jarno.ewijk@gmail.com>
2024-06-03 11:59:04 +0200

    Skeleton project push

---
+++ src/main/java/com/example/Gelderse_Kastelen_Java/GelderseKastelenJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gelderse_Kastelen_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelderseKastelenJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelderseKastelenJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gelderse_Kastelen_Java/GelderseKastelenJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gelderse_Kastelen_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelderseKastelenJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
