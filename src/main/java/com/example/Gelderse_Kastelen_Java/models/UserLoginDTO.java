88d930f81b5f2dc5b9b005436a00c1ce9b7b7c81
6EmptyBucket9 <jarno.ewijk@gmail.com>
2024-06-07 08:06:41 +0200

    Commit 4 Git bug fix
    Git bug fix

--- src/main/java/com/example/Gelderse_Kastelen_Java/models/UserLoginDTO.java
+++ src/main/java/com/example/Gelderse_Kastelen_Java/models/UserLoginDTO.java
@@ -1,27 +1,3 @@
-<<<<<<<< HEAD:src/main/java/com/example/Gelderse_Kastelen_Java/models/UserRegisterDTO.java
-package com.example.Gelderse_Kastelen_Java.models;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class UserRegisterDTO {   
-    private String userNaam;
-    private String userAchternaam;
-    private String userEmail;
-    private String userWachtwoord;
-    private String userRang;
-    private String userRol;
-}
-========
 package com.example.Gelderse_Kastelen_Java.models;
 
 
@@ -42,4 +18,3 @@ public class UserLoginDTO {
     private String userRole;
     private String userRang;
 }
->>>>>>>> d66913d7d0b9e155ca8da4cc052f7391956b0e7d:src/main/java/com/example/Gelderse_Kastelen_Java/models/UserLoginDTO.java
--- src/main/java/com/example/Gelderse_Kastelen_Java/models/UserRegisterDTO.java
+++ src/main/java/com/example/Gelderse_Kastelen_Java/models/UserRegisterDTO.java
@@ -1,4 +1,4 @@
-<<<<<<<< HEAD:src/main/java/com/example/Gelderse_Kastelen_Java/models/UserRegisterDTO.java
+
 package com.example.Gelderse_Kastelen_Java.models;
 
 
@@ -21,25 +21,3 @@ public class UserRegisterDTO {
     private String userRang;
     private String userRol;
 }
-========
-package com.example.Gelderse_Kastelen_Java.models;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class UserLoginDTO {
-    private String userEmail;
-    private String userName;
-    private String userRole;
-    private String userRang;
-}
->>>>>>>> d66913d7d0b9e155ca8da4cc052f7391956b0e7d:src/main/java/com/example/Gelderse_Kastelen_Java/models/UserLoginDTO.java
